36749f747cc468caa491c54d81aeeeff1ac1cfcc
Denis Kalyuzhnyy <denis.kaluzhnyy@gmail.com>
2023-05-28 23:24:56 +0300

    fix

--- .chezmoiscripts/run_onchange_before_001-install-ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-{{ if eq .chezmoi.osRelease.name "Ubuntu" }}
-sudo apt-get update
-sudo apt-get install python pip
-{{ else if eq .chezmoi.osRelease.name "Arch Linux" }}
-sudo pacman -Syy python python-pip ansible
-{{ end }}
