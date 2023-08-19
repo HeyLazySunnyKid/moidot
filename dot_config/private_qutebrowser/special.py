47dfe05317a682a3e611b2ef8c0503d8e9eead4c
Denis Kalyuzhnyy <denis.kalyuzhnyy@gmail.com>
2023-08-19 11:11:08 +0300

    fix colors

--- dot_config/private_qutebrowser/special.py
+++ dot_config/private_qutebrowser/special.py
@@ -1,2 +1,2 @@
 def set_colorscheme(c, everforest):
-    everforest.set(c, 'dark', 'soft') # options are dark/light and hard/medium/soft
+    everforest.set(c, 'light', 'hard') # options are dark/light and hard/medium/soft
--- dot_config/qutework/private_config/special.py
+++ dot_config/qutework/private_config/special.py
@@ -1,2 +1,2 @@
 def set_colorscheme(c, everforest):
-    everforest.set(c, 'light', 'hard') # options are dark/light and hard/medium/soft
+    everforest.set(c, 'dark', 'medium') # options are dark/light and hard/medium/soft
