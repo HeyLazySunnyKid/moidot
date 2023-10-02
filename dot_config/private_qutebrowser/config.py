d1719cdde7804b2e203f74e091bf6320ee1e09f2
Denis Kalyuzhnyy <denis.kalyuzhnyy@gmail.com>
2023-10-02 07:34:54 +0300

    add networks

--- dot_config/private_qutebrowser/config.py
+++ dot_config/private_qutebrowser/config.py
@@ -4,3 +4,8 @@ config.load_autoconfig(True)
 import everforest
 import special
 special.set_colorscheme(c, everforest) # options are dark/light and hard/medium/soft
+
+c.url.searchengines['g'] =  'https://www.google.com/search?q={}' # google
+c.url.searchengines['duck'] = 'https://duckduckgo.com/?q={}'
+c.url.searchengines['pypi'] = 'https://pypi.org/project/{}/'
+c.url.searchengines['repo'] = 'https://github.com/orgs/inDels/repositories?q={}&type=all&language=&sort=' # inDriver GitHub
