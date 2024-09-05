e3812b9059b5214e0627c29a9749a17b365d7005
Denis Kaliuzhnyy <deniskaspy@gmail.com>
2024-09-05 23:26:58 +0300

    remove qutebrowser & chrome

--- dot_config/private_qutebrowser/special.py
+++
@@ -1,2 +0,0 @@
-def set_colorscheme(c, everforest):
-    everforest.set(c, 'light', 'hard') # options are dark/light and hard/medium/soft
--- dot_config/qutework/private_config/special.py
+++
@@ -1,2 +0,0 @@
-def set_colorscheme(c, everforest):
-    everforest.set(c, 'dark', 'medium') # options are dark/light and hard/medium/soft
